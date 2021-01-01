dbd27f9a44caa1ba51ba61eb258d4ebfd0bfee8a
Nate Grobe <nategrobe@pop-os.localdomain>
2020-12-31 20:08:51 -0500

    board highlights which piece has been selected

--- game/Board.ts
+++ game/Board.ts
@@ -30,7 +30,10 @@ class Board {
       for (let j = 0; j < 8; j++) {
         const x = 125 * i;
         const y = 125 * j;
-        const colour = (i-j) % 2 == 0 ? color(255, 255, 255) : color(0, 0, 0);
+        let colour = (i-j) % 2 == 0 ? color(255, 255, 255) : color(0, 0, 0);
+        if (this.tiles[i + j*8].active) {
+          colour = (i-j) % 2 == 0 ? color(200, 200, 200) : color(55,55,55);
+        }
         fill(colour);
         square(x, y, 125);
       }
